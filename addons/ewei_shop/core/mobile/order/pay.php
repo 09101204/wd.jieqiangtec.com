2a5ee810e70523c7ecd838e58e0dae8d127f8f19
jieqiang <1569501393@qq.com>
2017-07-16 20:20:01 +0800

    订单id

--- addons/ewei_shop/core/mobile/order/pay.php
+++ addons/ewei_shop/core/mobile/order/pay.php
@@ -270,6 +270,7 @@ if ($operation == 'display' && $_W['isajax']) {
 //        $http_params['category_name'] =$order['id'];*/
 
         $_SESSION['prom']['item_id'] = $order_goods['goodsid'];
+        $_SESSION['order_id'] = $order['id'];
         $_SESSION['prom']['shop_id'] = '11';
         http_request(CPS_API,$_SESSION['prom']);
 		$ret = array();
